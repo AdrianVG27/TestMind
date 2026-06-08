30e020aa0dc2eb52e6a195d103782c32418e4406
AdrianVG27 <adrianvg2701@gmail.com>
2026-06-08 12:04:01 +0000

    Pruebas unitarias front

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/pruebas', function () {
+    return file_get_contents(public_path('pruebas/index.html'));
+});
+
 Route::get('/{any?}', function () {
     return file_get_contents(public_path('index.html'));
 })->where('any', '^(?!api).*$');
